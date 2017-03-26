ab9190861260650acb3a2b6c529a03c7ac92765c
ankur <garg.ankur@in.ibm.com>
2017-03-26 07:59:01 +0200

    test commit

---
+++ TestCarPool/src/ride/java/TestRunner.java
@@ -0,0 +1,14 @@
+package ride.java;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/ride/resources")
+public class TestRunner {
+	
+
+
+}
